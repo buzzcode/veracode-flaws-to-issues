62b6cf3bd01c0a2e3d9a0bbd4df463de752d1caf
Julian Totzek-Hallhuber <j.totzek@gmail.com>
2022-10-01 20:42:52 +0200

    update

--- policy.js
+++ policy.js
@@ -191,7 +191,7 @@ async function processPolicyFlaws(options, flawData) {
         
         var pr_link
 
-        if ( options.pr_commentID ){
+        if ( options.prCommentID ){
             let pr_link = `Veracode issue link to PR: https://github.com/repos/`+options.githubOwner+`/`+options.githubRepo+`/pull/`+options.pr_commentId
         }
 
