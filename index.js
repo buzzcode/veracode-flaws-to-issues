3112721eb372a807edf9c02358c5b312a71cf8ba
Julian Totzek-Hallhuber <j.totzek@gmail.com>
2022-10-01 17:47:18 +0200

    updates

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { constants } = require('fs/promises');
 
 const importFlaws = require('./importer').importFlaws;
 
